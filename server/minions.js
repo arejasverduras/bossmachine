4b1d7ffa011cd00722a075f84e2de546541baa9d
Arejas <62893479+arejasverduras@users.noreply.github.com>
2022-12-13 22:09:57 +0100

    minions Get route

--- server/minions.js
+++ server/minions.js
@@ -1,5 +1,18 @@
 const express = require('express');
 const minionsRouter = express.Router();
+const {
+    getAllFromDatabase,
+    getFromDatabaseById,
+    addToDatabase,
+    updateInstanceInDatabase,
+    deleteFromDatabasebyId,
+    deleteAllFromDatabase,} = require('./db')
+
+const minR = minionsRouter;
+
+minR.get('/', (req, res, next) => {
+    res.send(getAllFromDatabase('minions'));
+})
 
 
 
