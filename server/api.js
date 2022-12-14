1208f8ab8a99eb24c89d44e5b71083ff14efb6a4
Arejas <62893479+arejasverduras@users.noreply.github.com>
2022-12-14 12:26:38 +0100

    meetings routes except delete

--- server/api.js
+++ server/api.js
@@ -3,11 +3,11 @@ const apiRouter = express.Router();
 
 const minionsRouter = require('./minions');
 const ideasRouter = require('./ideas');
-// const meetingsRouter = require('./meetings');
+const meetingsRouter = require('./meetings');
 
 apiRouter.use('/minions', minionsRouter);
 apiRouter.use('/ideas', ideasRouter);
-// apiRouter.use('./meetings', meetingsRouter);
+apiRouter.use('/meetings', meetingsRouter);
 
 
 module.exports = apiRouter;
--- server/meetings.js
+++ server/meetings.js
@@ -1,6 +1,31 @@
 const express = require('express');
 const meetingsRouter = express.Router();
+const {
+    getAllFromDatabase,
+    getFromDatabaseById,
+    addToDatabase,
+    updateInstanceInDatabase,
+    deleteFromDatabasebyId,
+    deleteAllFromDatabase,
+    createMeeting
+    } = require('./db');
 
 
+mR = meetingsRouter;
+
+mR.get('/', (req, res, next) => {
+    res.send(getAllFromDatabase('meetings'));
+});
+
+mR.post('/', (req, res, next) => {
+    const newMeeting = createMeeting();
+    const addedMeeting = addToDatabase('meetings', newMeeting);
+    if (addedMeeting !== undefined)
+        {
+            res.status(201).send(addedMeeting);
+        } else {
+            res.status(400).send();
+        }
+});
 
 module.exports = meetingsRouter;
