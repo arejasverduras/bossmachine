1013c6059f9910e70b2b4ad3a53e075b831a844b
Arejas <62893479+arejasverduras@users.noreply.github.com>
2022-12-14 11:58:00 +0100

    declare all ideas routes + checkMillionDollarIdea Middleware

--- server/checkMillionDollarIdea.js
+++ server/checkMillionDollarIdea.js
@@ -1,4 +1,17 @@
-const checkMillionDollarIdea = () => {};
+const checkMillionDollarIdea = (req, res, next) => {
+    const numWeeks = Number(req.body.numWeeks);
+    const weeklyRevenue = Number(req.body.weeklyRevenue);
+    if (isNaN(numWeeks) || isNaN(weeklyRevenue)){
+        return res.status(400).send();
+    }
+
+    const turnover = numWeeks * weeklyRevenue;
+    if (turnover < 1000000){
+        res.status(400).send();
+    } else {
+        next();
+    }
+};
 
 // Leave this exports assignment so that the function can be used elsewhere
 module.exports = checkMillionDollarIdea;
--- server/ideas.js
+++ server/ideas.js
@@ -1,3 +1,4 @@
+const e = require('express');
 const express = require('express');
 const ideasRouter = express.Router();
 const {
@@ -6,7 +7,54 @@ const {
     addToDatabase,
     updateInstanceInDatabase,
     deleteFromDatabasebyId,
-    } = require('./db')
+    } = require('./db');
+const checkMillionDollarIdea = require('./checkMillionDollarIdea');
 
+const iR = ideasRouter;
+
+iR.get('/', (req, res, next) => {
+    res.send(getAllFromDatabase('ideas'));
+});
+
+iR.post('/', checkMillionDollarIdea, (req, res, next) => {
+    const createdIdea = addToDatabase('ideas', req.body);
+    if (createdIdea!== undefined){
+        res.status(201).send(createdIdea);
+    } else {
+        res.status(400).send();
+    }
+});
+
+iR.param('ideaId', (req, res, next, id) => {
+    const foundIdea = getFromDatabaseById('ideas', id);
+    if (foundIdea !== null && foundIdea !== undefined){
+        req.idea = foundIdea;
+        next();
+    } else {
+        res.status(404).send();
+    }
+});
+
+iR.get('/:ideaId', (req, res, next) => {
+    res.send(req.idea);
+});
+
+iR.put('/:ideaId', checkMillionDollarIdea, (req, res, next) => {
+    const updatedIdea = updateInstanceInDatabase('ideas', req.body);
+    if (updatedIdea !== null){
+        res.status(201).send(updatedIdea);
+    } else {
+        res.status(400).send();
+    }
+});
+
+iR.delete('/:ideaId', (req, res, next) => {
+    const deleted = deleteFromDatabasebyId('ideas', req.idea.id);
+    if (deleted){
+        res.status(204).send();
+    } else {
+        res.status(500).send();
+    }
+})
 
 module.exports = ideasRouter;
