753126e585d4b44cf728485c2a9e2429724c48cb
Arejas <62893479+arejasverduras@users.noreply.github.com>
2022-12-14 11:08:28 +0100

    declare all minions routes

--- server/minions.js
+++ server/minions.js
@@ -6,14 +6,54 @@ const {
     addToDatabase,
     updateInstanceInDatabase,
     deleteFromDatabasebyId,
-    deleteAllFromDatabase,} = require('./db')
+    } = require('./db')
 
 const minR = minionsRouter;
 
 minR.get('/', (req, res, next) => {
     res.send(getAllFromDatabase('minions'));
-})
+});
 
+minR.post('/', (req, res, next) => {
+    const created = addToDatabase('minions', req.body);
+    if (created !== undefined){
+        res.status(201).send(created);
+    } else {
+        res.status(400).send();
+    }
+});
+
+minR.param('minionId', (req, res, next, id) => {
+    const foundMinion = getFromDatabaseById('minions', id);
+    if (foundMinion !== null && foundMinion !== undefined){
+        req.minion = foundMinion;
+        next();
+    } else {
+        res.status(404).send();
+    }
+});
+
+minR.get('/:minionId', (req, res, next) => {
+    res.send(req.minion);
+});
+
+minR.put('/:minionId', (req, res, next) => {
+    const updated = updateInstanceInDatabase('minions', req.body);
+    if (updated !== null ){
+        res.status(201).send(updated);
+    } else {
+        res.status(400).send();
+    }
+});
+
+minR.delete('/:minionId', (req, res, next) => {
+    const deleted = deleteFromDatabasebyId('minions', req.minion.id);
+    if (deleted){
+        res.status(204).send();
+    } else {
+        res.status(404).send();
+    }
+})
 
 
 module.exports = minionsRouter;
